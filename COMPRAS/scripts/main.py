aa180b3aa42a662a396d5f52910bee89a77a9fb8
Thiago Martins <71234183+thiagofmart@users.noreply.github.com>
2022-05-11 18:12:42 -0300

    Add files via upload

---
+++ COMPRAS/scripts/main.py
@@ -0,0 +1,6 @@
+import pdfkit
+
+
+path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+pdfkit.from_file('./templates/pedido_SOLAR.html', "out.pdf", configuration=config)
