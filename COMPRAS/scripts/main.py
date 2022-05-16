fccf0a5de27d57b1b2187a3963fda5fc73eaa77b
Thiago Martins <71234183+thiagofmart@users.noreply.github.com>
2022-05-16 20:24:12 -0300

    Delete COMPRAS/scripts directory

--- COMPRAS/scripts/main.py
+++
@@ -1,6 +0,0 @@
-import pdfkit
-
-
-path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-pdfkit.from_file('./templates/pedido_SOLAR.html', "out.pdf", configuration=config)
